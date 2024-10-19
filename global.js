bb7b46c2e0df126da7ec5a6d033bc291b9c12873
chm011 <chm011@ucsd.edu>
2024-10-19 00:36:51 -0700

    revision

--- global.js
+++ global.js
@@ -15,9 +15,9 @@ const ARE_WE_HOME = normalizePath(location.pathname) === '';
 
 // Array of pages for the navigation menu
 let pages = [
-    { url: '/portfolio/index.html', title: 'Home' },
-    { url: '/portfolio/projects/index.html', title: 'Projects' },
-    { url: '/portfolio/resume/index.html', title: 'Resume' },
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
     { url: '/portfolio/contacts/index.html', title: 'Contacts' },
     { url: 'https://github.com/chm011', title: 'Github Profile' }
   ];
