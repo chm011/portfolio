a52d743362fb90bc7c9b7ddde324209a4c2b3122
chm011 <chm011@ucsd.edu>
2024-10-18 17:22:11 -0700

    More updates

--- global.js
+++ global.js
@@ -23,6 +23,7 @@ let pages = [
     {url: 'projects/', title: 'Projects'},
     {url: 'resume/', title: 'Resume'},
     {url: 'contacts', title: 'Contacts'}
+    {url: 'https://github.com/chm011', title: 'Github Profile'}
 ]
 
 let nav = document.createElement('nav');
@@ -35,30 +36,36 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    //TODO create link and add it to nav
+    
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    //create link and add it to nav
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    //highlight current page
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+
+    //opening external links in a new tab
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
 }
 
-url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-
-
-//create link and add it to nav
-let a = document.createElement('a');
-a.href = url;
-a.textContent = title;
-nav.append(a);
-
-//highlight current page
-a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname
-  );
-
-
-//opening external links in a new tab
-if (a.host !== location.host) {
-    a.target = '_blank';
-  }
+const select = document.querySelector('#theme-swticher');
 
 select.addEventListener('input', function (event) {
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
     console.log('color scheme changed to', event.target.value);
 });  
+
+if ('colorScheme' in localStorage) {
+    const savedScheme = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', savedScheme);
+    select.value = savedScheme;
+  }
