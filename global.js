234458d2252a84e972bc0d93b6c17d0e990eac55
chm011 <chm011@ucsd.edu>
2024-10-19 00:13:38 -0700

    Update global.js

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ const ARE_WE_HOME = normalizePath(location.pathname) === '';
 
 // Array of pages for the navigation menu
 let pages = [
-    { url: '/portfolio/index.html', title: 'Home' },
+    { url: 'index.html', title: 'Home' },
     { url: 'https://chm011.github.io/projects/index.html', title: 'Projects' },
     { url: '/portfolio/resume/index.html', title: 'Resume' },
     { url: '/portfolio/contacts/index.html', title: 'Contacts' },
